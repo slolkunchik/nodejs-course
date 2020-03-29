84f782e14dbf91cb41eff57ed186c780d56799a5
Marina Sevkovich <slolkunchik@gmail.com>
2020-03-29 16:05:04 +0300

    feat: add new line separator for multiple input

--- task1/CodingTransformer.js
+++ task1/CodingTransformer.js
@@ -1,6 +1,7 @@
 const actionUtil = require('./utils/actionUtil');
 const stream = require('stream');
 const program = require('./program');
+const os = require('os');
 
 module.exports = class CodingTransformer extends stream.Transform {
   constructor(action) {
@@ -11,6 +12,10 @@ module.exports = class CodingTransformer extends stream.Transform {
     this.push(
       actionUtil(this.action, chunk.toString(), parseInt(program.shift, 10))
     );
+    callback();
+  }
+  _flush(callback) {
+    this.push(os.EOL);
     callback();
   }
 };
